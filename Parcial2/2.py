8c5e89c08cba3242f084a7e276d4ae1a0dd3654f
NickTTD <nico-diaz@hotmail.es>
2023-11-06 21:21:57 -0300

    Parcial2

--- Parcial2/2.py
+++ Parcial2/2.py
@@ -18,15 +18,15 @@ def insert_vertices():
         'Chewbacca',
         'R2-D2',
         'C-3PO',
-        'Boba Fett'
-        'BB-8'
-        'Kylo Ren'
+        'Boba Fett',
+        'BB-8',
+        'Kylo Ren',
         'Rey',
     ]
     for character in characters:
         grafo_personajes.insert_vertex(character)
 insert_vertices()
-def insert_arists():
+def insert_aristas():
     grafo_personajes.insert_arist('Chewbacca', 'Obi-Wan Kenobi', 2)
     grafo_personajes.insert_arist('Obi-Wan Kenobi', 'Darth Vader', 3)
     grafo_personajes.insert_arist('C-3PO', 'Luke Skywalker', 4)
@@ -35,38 +35,42 @@ def insert_arists():
     grafo_personajes.insert_arist('Han Solo', 'Princess Leia', 2)
     grafo_personajes.insert_arist('Boba Fett', 'Darth Vader', 3)
     grafo_personajes.insert_arist('Darth Vader', 'Han Solo', 6)
-    grafo_personajes.insert_arist('Yoda', 'Luke Skywalker', 3)
+    #grafo_personajes.insert_arist('Yoda', 'Luke Skywalker', 3)
     grafo_personajes.insert_arist('R2-D2', 'Luke Skywalker', 3)
     grafo_personajes.insert_arist('Han Solo', 'Kylo Ren', 3)
     grafo_personajes.insert_arist('Kylo Ren', 'Luke Skywalker', 2)
     grafo_personajes.insert_arist('Kylo Ren', 'Rey', 3)
     grafo_personajes.insert_arist('BB-8', 'Rey', 3)
     grafo_personajes.insert_arist('BB-8', 'Luke Skywaler', 3)
-insert_arists()
+insert_aristas()
 
 #b) hallar el árbol de expansión minino y determinar si contiene a Yoda;
+
 #Quise hacerlo de una manera que quede mas prolijo el print del árbol de expansión minimo usando
 #mi kruskal modificado (Como hice en el tp6 ejercicio 14), pero no logré hacerlo funcionar
 #por lo que usé el mismo código que hicimos en clase + agregué la lógica para yoda...
 
-#Pero incluso si yoda no está en al árbol, es decir, es un vértice sin ninguna arista (Comentar linea 35), también cuenta como si estuviera ahi...
-#Tampoco entiendo por que Boba fett (y otros personajes) quedan en el primer arbol, separado del segundo árbol, cuando en teoría Boba está conectado a Darth Vader (Linea 36)
-#
+#Si yoda está por si solo, es decir, no tiene ninguna arista (Porque la linea 38 está comentada), devuelve que no está en ningún arbol (Aunque podría)
+#considerarse que un vertice solo es un árbol, creo que no tendría gracia, por lo que hice que si es parte de uno de los árboles de expansión posibles, detecte
+#la arista.
+#(Des comentar linea 38 para probar)
 def b():
     bosque = grafo_personajes.kruskal_original()
     yoda_present = False
+
+    print('-----arboles-----')
     for arbol in bosque:
-        if "Yoda" in arbol:
-            yoda_present = True
-        print('-----arbol------')
+        print(arbol)
+
+        # Verifica si Yoda está presente en alguna arista
         for nodo in arbol.split(';'):
-            print(nodo)
+            if "Yoda" in nodo and '-' in nodo:  # Verifica que haya una arista que incluya a Yoda
+                yoda_present = True
 
     if yoda_present:
-        print("Yoda esta en el arbol de exp mininma")
+        print("Yoda está en algún árbol de expansión mínima")
     else:
-        print("Yoda no esta en el arbol de exp minima")
-
+        print("Yoda no está en ningún árbol de expansión mínima")
 
 
 #c) determinar cuál es el número máximo de episodio que comparten dos personajes, y quienes son.
